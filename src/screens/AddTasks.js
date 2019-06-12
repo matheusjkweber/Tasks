0651a7bf2f108c7d38053e3e4fb1dcba9bebad52
Matheus Weber <matheus.weber@ibm.com>
2019-06-12 09:45:09 -0300

    Add create task component

--- src/screens/AddTasks.js
+++ src/screens/AddTasks.js
@@ -7,7 +7,8 @@ import {
     DatePickerIOS,
     StyleSheet,
     TouchableWithoutFeedback,
-    TouchableOpacity
+    TouchableOpacity,
+    Alert
 } from 'react-native'
 import moment from 'moment'
 import 'moment/locale/pt-br'
@@ -16,5 +17,85 @@ import commonStyles from '../commonStyles'
 const initialState = { desc: '', date: new Date() }
 
 export default class AddTask extends Component {
-    
+    state = {...initialState}
+
+    save = () => {
+        if(!this.state.desc.trim()) {
+            Alert.alert("Dados Inválidos", "Informe uma descrição para a tarefa!")
+            return
+        }
+ 
+        const data = { ...this.state }
+        this.props.onSave(data)
+        this.setState({ ...initialState })
+    }
+
+    render() {
+        return (
+            <Modal onRequestClose={this.props.onCancel}
+                visible={this.props.isVisible}
+                animationType='slider' transparent={true}>
+                    <TouchableWithoutFeedback onPress={this.props.onCancel}>
+                        <View style={styles.offset}>
+
+                        </View>
+                    </TouchableWithoutFeedback>
+                    <View style={styles.container}>
+                        <Text style={styles.header}>Nova Tarefa</Text>
+                        <TextInput placeholder='Descrição' style={styles.input}
+                            onChangeText={desc => this.setState({ desc })}
+                            value={this.state.desc}/>
+                        <DatePickerIOS mode='date' date={this.state.date} onDateChange={date => this.setState({ date })}/>
+                        <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+                            <TouchableOpacity onPress={this.props.onCancel} style={styles.button}>
+                                <Text>Cancelar</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={this.save} style={styles.button}>
+                                <Text>Salvar</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                    <TouchableWithoutFeedback onPress={this.props.onCancel}>
+                        <View style={styles.offset}>
+                            
+                        </View>
+                    </TouchableWithoutFeedback>
+                </Modal>
+        )
+    }
 }
+
+var styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        justifyContent: 'space-between'
+    },
+    offset: {
+        flex: 1,
+        backgroundColor: 'rgba(0,0,0,0.7)'
+    },
+    button: {
+        margin: 20,
+        marginRight: 30,
+        color: commonStyles.colors.default
+    },
+    header: {
+        fontFamily: commonStyles.fontFamily,
+        backgroundColor: commonStyles.colors.default,
+        color: commonStyles.colors.secondary,
+        textAlign: 'center',
+        padding: 15,
+        fontSize: 15,
+    },
+    input: {
+        fontFamily: commonStyles.fontFamily,
+        width: '90%',
+        height: 40,
+        marginTop: 10,
+        marginLeft: 10,
+        backgroundColor: 'white',
+        borderWidth: 1,
+        borderColor: '#e3e3e3',
+        borderRadius: 6
+    }
+})
--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -14,6 +14,9 @@ import todayImage from '../../assets/imgs/today.jpg'
 import commonStyles from '../commonStyles'
 import Task from '../components/Task'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import ActionButton from 'react-native-action-button'
+import AddTasks from './AddTasks'
+
 
 export default class Agenda extends Component {
     
@@ -32,7 +35,20 @@ export default class Agenda extends Component {
             {id: Math.random(), desc: "Tarefa 2", estimateAt: new Date(), doneAt: new Date()}
         ],
         visibleTasks: [],
-        showDoneTasks: true
+        showDoneTasks: true,
+        showAddTask: false
+    }
+
+    addTask = task => {
+        const tasks = [...this.state.tasks]
+        tasks.push({
+            id: Math.random(),
+            desc: task.desc,
+            estimateAt: task.date,
+            doneAt: null
+        })
+
+        this.setState({ tasks, showAddTask: false }, this.filterTasks)
     }
 
     filterTasks = () => {
@@ -70,6 +86,9 @@ export default class Agenda extends Component {
     render() {
         return (
             <View style={styles.container}>
+                <AddTasks isVisible={this.state.showAddTask}
+                    onSave={this.addTask}
+                    onCancel={() => this.setState({ showAddTask: false })}/>
                 <ImageBackground source={todayImage} style={styles.background}>
                     <View style={styles.iconBar}>
                         <TouchableOpacity onPress={this.toggleFilter}>
@@ -88,7 +107,7 @@ export default class Agenda extends Component {
 
                     </FlatList>
                 </View>
-
+                <ActionButton buttonColor={commonStyles.colors.today} onPress={() => { this.setState({ showAddTask: true})}}/>
             </View>
         )
     }
